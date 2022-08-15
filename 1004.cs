529b9c882c9710751d4c0d073e126af0a4d2d501
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-08-15 12:37:34 -0300

    Simple Product

---
+++ 1004.cs
@@ -0,0 +1,14 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("PROD = {0}", A * B);
+    
+        
+    }
+
+}
