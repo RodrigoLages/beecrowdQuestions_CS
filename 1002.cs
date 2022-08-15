2b6df635b4a166bb6f51456142d0c5c0065a1c2f
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-08-15 12:32:07 -0300

    Area of a Circle

---
+++ 1002.cs
@@ -0,0 +1,12 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        double radius = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("A={0:0.0000}", radius * radius * 3.14159);
+
+    }
+
+}
