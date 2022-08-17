e097293b56a58c76fb06a5e419a7f067071b3d48
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2022-08-17 09:54:20 -0300

    Odd Numbers

---
+++ 1067.cs
@@ -0,0 +1,16 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+
+    }
+
+}
